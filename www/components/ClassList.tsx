3c416dbd0320da545421ae2a18fa916d075f353b
Nick Dieff <nick.dieff@gmail.com>
2019-04-11 15:45:07 -0400

    updated class list

--- www/components/ClassList.tsx
+++ www/components/ClassList.tsx
@@ -4,6 +4,8 @@ import { useQuery } from "react-apollo-hooks";
 import ErrorMessage from "./ErrorMessage";
 import { MY_CLASSES } from "./__generated__/MY_CLASSES";
 import { classesRoute } from "../routes";
+import styled from "styled-components";
+import Link from "next/link";
 
 const MY_CLASSES_QUERY = gql`
   query MY_CLASSES {
@@ -18,6 +20,34 @@ const MY_CLASSES_QUERY = gql`
   }
 `;
 
+const List = styled.div`
+  width: 20rem;
+  background-color: #e8e8e8;
+`;
+
+const ListItem = styled.div`
+  height: 2rem;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-around;
+
+  :hover {
+    background-color: #e0e0e0;
+    box-shadow: 0 0.25rem 0.3rem #bababa;
+  }
+
+  :active {
+    box-shadow: inset 0.25rem 0.25rem 0.5rem #bababa;
+  }
+`;
+
+const InnerLink = styled.a`
+  width: 100%;
+  padding-left: 7rem;
+  margin-top: 0.3rem;
+  text-decoration: none;
+`;
+
 const ClassList: FunctionComponent = () => {
   const { data, loading, error } = useQuery<MY_CLASSES>(MY_CLASSES_QUERY);
 
@@ -29,17 +59,19 @@ const ClassList: FunctionComponent = () => {
     return <div>Loading...</div>;
   }
 
-  const Link = classesRoute.Link;
+  const ClassesLink = classesRoute.Link;
   //console.log(test);
 
   return (
-    <div>
+    <List>
       {data.myClasses.map(cl => (
-        <Link path={`/classes/${cl.class.id}`} prefetch={false}>
-          <a>{cl.class.name}</a>
-        </Link>
+        <ListItem key={cl.id}>
+          <ClassesLink path={`/classes/${cl.class.id}`}>
+            <InnerLink>{cl.class.name}</InnerLink>
+          </ClassesLink>
+        </ListItem>
       ))}
-    </div>
+    </List>
   );
 };
 
