52a35ab1b7ddf05f9f1b5d8187bcc182fd21b3c6
Nick Dieff <nick.dieff@gmail.com>
2019-04-18 12:08:19 -0400

    updated fileStorage helper to create public bucket, ahndle perms

--- api/src/lib/fileStorage.ts
+++ api/src/lib/fileStorage.ts
@@ -3,12 +3,13 @@ import { Stream } from 'stream'
 import { S3 } from 'aws-sdk'
 import { randomBytes } from 'crypto'
 import { promisify } from 'util'
+import console = require('console')
 
 const makeRandBytes = promisify(randomBytes)
 
 const setupClient = () => {
   if (!process.env.FILE_STORAGE_ENDPOINT) {
-    throw new Error('No Object Storage Endpoint')
+    throw new Error('Missing ENV VAr $FILE_STORAGE_ENDPOINT')
   }
   return new AWS.S3({
     accessKeyId: process.env.FILE_STORAGE_ID,
@@ -33,30 +34,91 @@ export class ObjectStorage {
   client: S3
   constructor() {
     this.client = setupClient()
+    // setup the public bucket if it does not exist
+    ;(async () => {
+      if (!(await this.storageHasBucket(this.getPublicBucket()))) {
+        await this.createPublicBucket(this.getPublicBucket())
+      }
+    })().catch(console.warn)
   }
 
-  getUrl(bucket: string, key: string) {
+  private async createPublicBucket(name: string) {
+    await this.client
+      .createBucket({ Bucket: name, ACL: 'public-read' })
+      .promise()
+    await this.client
+      .putBucketPolicy({
+        Bucket: name,
+        Policy: JSON.stringify({
+          Statement: [
+            {
+              Action: ['s3:GetBucketLocation', 's3:ListBucket'],
+              Effect: 'Allow',
+              Principal: {
+                AWS: ['*'],
+              },
+              Resource: [`arn:aws:s3:::${name}`],
+            },
+            {
+              Action: ['s3:GetObject'],
+              Effect: 'Allow',
+              Principal: {
+                AWS: ['*'],
+              },
+              Resource: [`arn:aws:s3:::${name}/*`],
+            },
+          ],
+          Version: '2012-10-17',
+        }),
+      })
+      .promise()
+  }
+
+  private async storageHasBucket(bucket: string): Promise<boolean> {
+    try {
+      const res = await this.client.getBucketAcl({ Bucket: bucket }).promise()
+      if (res.$response.data) {
+        return true
+      }
+    } catch (err) {}
+    return false
+  }
+
+  private getUrl(bucket: string, key: string) {
     return `${this.client.endpoint.href}/${bucket}/${key}`
   }
 
-  uploadFile = async (
-    file: Stream,
-    type: string,
-  ): Promise<IUploadFileResult> => {
-    const Key = (await makeRandBytes(32)).toString('hex')
-    const result = await this.client
-      .upload({ Body: file, Bucket: 'test', Key, ContentType: type })
-      .promise()
-    return { url: result.Location, key: Key, bucket: 'test' }
+  getPublicBucket(): string {
+    if (!process.env.PUBLIC_STORAGE_BUCKET) {
+      throw new Error('Missing env var $PUBLIC_STORAGE_BUCKET')
+    }
+    return process.env.PUBLIC_STORAGE_BUCKET
   }
 
-  cloneFile = async (fileKey: string): Promise<IUploadFileResult> => {
+  async uploadFile(
+    bucket: string,
+    incomingFile: Stream,
+    type: string,
+  ): Promise<IUploadFileResult> {
+    const Key = (await makeRandBytes(32)).toString('hex')
+    const result = await this.client
+      .upload({ Body: incomingFile, Bucket: bucket, Key, ContentType: type })
+      .promise()
+    return { url: result.Location, key: Key, bucket: bucket }
+  }
+
+  async cloneFile(
+    fileKey: string,
+    oldBucket: string,
+    newBucket: string,
+  ): Promise<IUploadFileResult> {
     const newKey = (await makeRandBytes(32)).toString('hex')
     await this.client
       .copyObject({
-        Bucket: 'test',
+        Bucket: newBucket,
         Key: newKey,
-        CopySource: `/test/${fileKey}`,
+        CopySource: `/${oldBucket}/${fileKey}`,
+        ACL: 'public-read',
       })
       .promise()
     return {
@@ -69,9 +131,9 @@ export class ObjectStorage {
   /**
    * Returns whether or not the file was successfully deleted.
    */
-  deleteFile = async (fileKey: string): Promise<boolean> => {
+  async deleteFile(bucket: string, fileKey: string): Promise<boolean> {
     const result = await this.client
-      .deleteObject({ Bucket: 'test', Key: fileKey })
+      .deleteObject({ Bucket: bucket, Key: fileKey })
       .promise()
     if (result) {
       return true
